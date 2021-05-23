039efa1d0bdac9e1e6fc36c00183e49ab758716d
IvanVolkov42 <ivanv@choosewithme.com>
2021-05-23 20:37:17 +0700

    Learning Javascript

--- src/alert.js
+++ src/alert.js
@@ -1,2 +1,54 @@
 
-    alert('Я Javascript');
+    //alert('Я Javascript')
+    /*let admin;
+    let name;
+    name='Джон';
+    admin=name;
+    //alert(admin);
+    let currentUserName =prompt("What is your name?");
+    alert(currentUserName);*/
+    //let a = +prompt("Первое число",1);
+    //let b = +prompt("Второе число",2);
+    //alert(a + b);
+    /*let JavaName = prompt("Какое официальное название Javascript?");
+    if(JavaName == "ECMAscript")
+    {
+        alert("Верно");
+    } else
+    {
+        alert("Не знаете? ECMAScript");
+    }*/
+   /* let userNumber = prompt("Введите число");
+    if(userNumber>0)
+    {
+        alert("1");
+    } else if (userNumber<0) {
+        alert("-1");
+    } else if (userNumber==0)
+    {
+     alert("0");
+    }*/
+    //let result = a + b;
+    //(result < 4) ? alert("Мало") : alert("Много");
+    /*let login = prompt("Введите логин");
+    let message = (login == "Сотрудник") ? 'Привет':
+        (login =="Директор") ? " здравстуйте" :
+            (login == "") ? "Нет логина" : "";
+    alert(message);*/
+    let age = prompt("Введите свой возраст");
+    /*if(age < 14 || age >90)
+    {
+        alert(age);
+    }*/
+    let ageValue;
+    if (!(age >= 14 && age <=90))
+    {
+        alert(age);
+    }
+
+
+
+
+
+
+
