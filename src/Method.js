c13f0cf401e06adf0d89974bb36996b5871c51b1
IvanVolkov42 <ivanv@choosewithme.com>
2021-05-31 14:25:13 +0700

    Learning Javascript

--- src/Method.js
+++ src/Method.js
@@ -30,3 +30,32 @@ let ladder = {
     }
 };
 ladder.up().up().down().up().showStep();
+// task 3 Калькулятор через конструктор
+function Calculator () {
+    this.read = function () {
+        this.a = +prompt("Введите 1 число");
+        this.b = +prompt("Введите 2 число");
+    };
+    this.sum = function () {
+        return (this.a + this.b);
+    };
+    this.mul = function () {
+        return (this.a * this.b);
+    };
+}
+let calculator = new Calculator();
+calculator.read();
+alert (` Сумма введеных чисел равна = ${calculator.sum()}`);
+alert (` Произведение введеных чисел равно = ${calculator.mul()}`);
+// task 4 Accumulator
+function Accumulator (startingValue) {
+    this.value = startingValue;
+    this.readValue = function () {
+        this.value += +prompt(`Сколько прибавить к ${userValue} ?`);
+    };
+}
+let userValue = +prompt("Введите число")
+let accumulator = new Accumulator(userValue);
+accumulator.readValue();
+accumulator.readValue();
+alert(accumulator.value);
