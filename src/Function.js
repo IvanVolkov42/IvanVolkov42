dbe0b43f1a83a39d33e4dd2ad6731b7f8fdd54c4
IvanVolkov42 <ivanv@choosewithme.com>
2021-06-03 00:16:41 +0700

    Learning Javascript

--- src/Function.js
+++ src/Function.js
@@ -56,3 +56,31 @@ ask(
     () => alert("Вы согласны"),
     () => alert("Вы не согласны")
 );
+// task 6 Фильтрация
+function inBetween(a,b) {
+    return function (x) {
+        return x>=a && x<= b;
+    };
+}
+function inArray(arr) {
+    return function (x) {
+        return arr.includes(x);
+    };
+}
+let arr = [1,2,3,4,5,6,7];
+alert(arr.filter(inArray([1,2,10])) );
+alert(arr.filter(inBetween(3,6)) );
+
+//task 7 сортировка по полю
+let users = [
+    { name: "John", age: 20, surname: "Johnson" },
+    { name: "Pete", age: 18, surname: " Peterson" },
+    { name: "Ann", age: 19, surname: "Hathaway" }
+];
+function byField(fieldName) {
+    return (a,b) => a[fieldName] < b[fieldName] ? 1 : -1;
+}
+users.sort(byField("name"));
+users.forEach(user => alert(user.name));
+users.sort(byField("age"));
+users.forEach(user => alert(user.name));
